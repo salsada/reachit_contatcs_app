17f0d4f3f351b9c3c0c4337cb8a7f11e774ecd9a
Alberto Botero <alberto.botero@cpcc>
2016-03-15 21:32:57 -0400

    Setting up the Projects

--- server/config/express.js
+++ server/config/express.js
@@ -12,6 +12,7 @@ module.exports = function (app, config){
     }
 
     app.set('views', config.rootPath + '/server/views');
+    app.set('view engine', 'jade');
     app.use(logger('dev'));
     app.use(cookieParser());
     app.use(bodyParser.urlencoded({extended:true}));
@@ -26,4 +27,4 @@ module.exports = function (app, config){
         }
     ));
     app.use(express.static(config.rootPath + '/public'));
-}
+};
